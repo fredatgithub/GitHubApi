02b79326206a993c090a69ca4f1de5259910044c
fred <domaine75@gmail.com>
2015-12-21 11:44:10 +0100

    Update FormMain.cs

--- GitHubApi/FormMain.cs
+++ GitHubApi/FormMain.cs
@@ -557,7 +557,7 @@ namespace GitHubApi
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
